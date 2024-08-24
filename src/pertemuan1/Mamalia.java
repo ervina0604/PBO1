4a3a864f09434facfcc0f494d8214358acfd962b
ervina0604 <115573870+ervina0604@users.noreply.github.com>
2024-08-24 13:24:15 +0700

    first commit

---
+++ src/pertemuan1/Mamalia.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author HP
+ */
+public class Mamalia extends Hewan {
+    private String jenisRambut;
+    private String jenisMakanan;
+
+    /**
+     * @return the jenisRambut
+     */
+    public String getJenisRambut() {
+        return jenisRambut;
+    }
+
+    /**
+     * @param jenisRambut the jenisRambut to set
+     */
+    public void setJenisRambut(String jenisRambut) {
+        this.jenisRambut = jenisRambut;
+    }
+
+    /**
+     * @return the jenisMakanan
+     */
+    public String getJenisMakanan() {
+        return jenisMakanan;
+    }
+
+    /**
+     * @param jenisMakanan the jenisMakanan to set
+     */
+    public void setJenisMakanan(String jenisMakanan) {
+        this.jenisMakanan = jenisMakanan;
+    }
+}
---
+++ src/pertemuan1/Pisces.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author HP
+ */
+public class Pisces extends Hewan {
+    private String temperaturTubuh;
+    private String tipeSirip;
+
+    public String getTemperaturTubuh() {
+        return temperaturTubuh;
+    }
+
+    public void setTemperaturTubuh(String temperaturTubuh) {
+        this.temperaturTubuh = temperaturTubuh;
+    }
+
+    public String getTipeSirip() {
+        return tipeSirip;
+    }
+
+    public void setTipeSirip(String tipeSirip) {
+        this.tipeSirip = tipeSirip;
+    }
+}
